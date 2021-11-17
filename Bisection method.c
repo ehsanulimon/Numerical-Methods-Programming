ab38abc416cfa9b563d1bc6e75ced442748d7abe
Ehsanul Mostafa Imon <mostafaimon012020@gmail.com>
2021-11-17 16:07:55 +0600

    17.11.21

--- Bisection method.c
+++ Bisection method.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 #include<conio.h>
 #include<math.h>
-#define f(x) (x*x) - (4*x) - 10
-#define E 0.000001
+#define f(x) a*(x*x) - (b*x) - c
+#define E 0.001
 main()
 {
     float x1,x2,f0,f1,f2,x0,root;
-    int i=1;
-
+    int i=1,a,b,c;
+    printf("Enter a b c : ");
+    scanf("%d %d %d",&a,&b,&c);
+    
     printf("Enter two limits : ");
     scanf("%f%f",&x1,&x2);
     f1=f(x1);
@@ -21,7 +23,9 @@ main()
     {
     	   //bisection position formula
         x0=(x1+x2)/2;
+        
         f0=f(x0);
+        
         if(f1*f0<0)
             x2=x0;
         else
@@ -30,7 +34,7 @@ main()
             f1=f0;
         }
         printf("\nIteration %d %f %f",i,x1,x2);
-        if(fabs((x2-x1)/x2)<E)
+        if(fabs((x2-x1)/x2)<E) //The C library function double fabs(double x) returns the absolute value of x.
         {
             root = (x1+x2)/2;
             printf("\nFinal iteration=%d    Root = %f",i+1,root);
--- False Position method.c
+++ False Position method.c
@@ -2,7 +2,8 @@
 #include<conio.h>
 #include<math.h>
 #define f(x) (x*x) - x- 2  //example 6.5
-#define E 0.000001
+
+#define E 0.001
 main()
 {
     float x1,x2,f0,f1,f2,x0,root;
@@ -18,30 +19,32 @@ main()
         printf("\nX1 & x2 do not bracket any root");
         goto stop;
     }
-    while(i<100)
+    do
     {
          //false position formula
-         x0= x1-(f1*(x2-x1))/(f2-f1);
+        // x0= x1-(f1*(x2-x1))/(f2-f1);
+         x0 = x1- (f1*x2 -f1*x1)/(f2-f1);
       
         f0=f(x0);
-        if(f1*f0<0)
+   
+        
+        if(f1*f0<0){
             x2=x0;
+            f2=x0;	
+		}
+        
         else
         {
             x1=x0;
             f1=f0;
         }
-        printf("\nIteration %d %f %f",i,x1,x2);
-        if(fabs((x2-x1)/x2)<0)
-        {
-            root = (x1+x2)/2;
-            printf("\nFinal iteration=%d    Root = %f",i+1,root);
-            goto stop;
-        }
-        else
-            i++;
-
-    }
+         i++;
+     printf("\nIteration %d %f %f",i,x1,x2);
+    }while(fabs((x2-x1)/x2)<E);
+	
+	 root = (x1+x2)/2;
+     printf("\nFinal iteration=    Root = %f",root);
+ 
     stop:
     getch();
 }
